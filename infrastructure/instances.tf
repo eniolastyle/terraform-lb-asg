6f74ac19d21480d9e6af3dd76ef23508082d8c73
eniolastyle <amiolastyle@gmail.com>
2023-04-19 13:29:41 +0100

    terraform-lb-asg feature-a

--- infrastructure/instances.tf
+++ infrastructure/instances.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "nginx-server" {
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.ec2-server.key_name
   vpc_security_group_ids = [aws_security_group.general-sg.id]
-  user_data              = data.template_file.nginx_data_script.rendered
+  user_data              = base64encode(data.template_file.nginx_data_script.rendered)
 
   tags = {
     "Name" = "nginx-server"
@@ -17,7 +17,7 @@ resource "aws_instance" "apache-server" {
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.ec2-server.key_name
   vpc_security_group_ids = [aws_security_group.general-sg.id]
-  user_data              = data.template_file.apache_data_script.rendered
+  user_data              = base64encode(data.template_file.apache_data_script.rendered)
 
   tags = {
     "Name" = "apache-server"
